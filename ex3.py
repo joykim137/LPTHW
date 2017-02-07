b1ad9128f7a99138b1e3abad65d7a9fab321640b
Joy Kim <joykim@seoulchristian.org>
2017-02-07 11:38:11 +0900

    Add ex3.py

---
+++ ex3.py
@@ -0,0 +1,32 @@
+# prints phrase in quotation marks
+print "I will now count my chickens:"
+
+# prints phrases in quotation marks and result of cacluation of equations
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# prints phrase in quotation marks
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# prints phrase in quotation marks
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# prints the verity of the equation
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# prints phrases in quotation marks and result of cacluation of equations
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+# prints phrase in quotation marks
+print "Oh, that's why it's False."
+
+# prints phrase in quotation marks
+print "How about some more."
+
+# prints phrases in quotation marks and the verity of the equations
+print "Is it greater?", 5.0 >= -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
